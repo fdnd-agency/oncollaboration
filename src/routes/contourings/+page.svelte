2387732dfff613f4863dbdbbac610d5fa246894c
zoepje <zoe.penner@hva.nl>
2025-01-15 21:38:44 +0100

    push oncollaboration
    release-candidate

---
+++ src/routes/contourings/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+  import {Search, Filter, ContouringOverview} from "$lib/index.js";
+  export let data;
+  let vt = true //What does this mean
+</script>
+
+<main>
+  <Search />
+
+  <Filter activeCategory={data.category} currentPage="contourings"/>
+
+  {#if data.category != 'all'}
+  <h2>Contouring results for {data.category}</h2>
+  {:else}
+  <h2>All contourings</h2>
+  {/if}
+  
+  <ul>
+    {#each data.contourings as contouring}
+    <li>
+      <ContouringOverview {...contouring} {vt}/>
+    </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    width: 100%;
+  }
+
+  h2 {
+    margin: 1em 0 0 1em;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    margin: 2em 1em 0 1em;
+  }
+
+  @media screen and (min-width: 1080px) {
+    ul {
+      justify-content: left;
+    }
+  }
+</style>
---
+++ src/routes/webinars/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+  import {Search, Filter, WebinarOverview, FeaturedWebinar} from "$lib/index.js";
+  export let data;
+</script>
+
+<main>
+  <Search />
+
+  <Filter activeCategory={data.category} currentPage="webinars" />
+
+  {#if data.category != 'all'}
+  <h2>Webinar results for {data.category}</h2>
+  {:else}
+  {#each data.FeaturedWebinars as featuredWebinar}
+    <FeaturedWebinar {...featuredWebinar}/>
+  {/each}
+  <h2>All webinars</h2>
+  {/if}
+  
+  <ul>
+    {#each data.webinars as webinar}
+    <li>
+      <WebinarOverview {...webinar}/>
+    </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    width: 100%;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+  }
+
+  li {
+    max-width: var(--card-max-width);
+  }
+
+  @media only screen and (min-width: 600px) {
+    h2 {
+      margin-left: 1em;
+      margin-top: 2em;
+    }
+
+    ul {
+      margin: 2em 1em 0 1em;
+      gap: 50px;
+    }
+  }
+</style>
