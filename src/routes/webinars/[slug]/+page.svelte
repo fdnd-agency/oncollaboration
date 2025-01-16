a2156ac06f8382a11b7327d69e336d80d52dfdeb
zoepje <zoe.penner@hva.nl>
2025-01-16 18:26:17 +0100

    load right sized images in

--- src/routes/webinars/[slug]/+page.svelte
+++ src/routes/webinars/[slug]/+page.svelte
@@ -88,7 +88,7 @@
 
     {#each data.webinar.speakers as speaker}
       <section class="speaker-info">
-        <img src="https://fdnd-agency.directus.app/assets/{speaker.avl_speakers_id.profile_picture}?format=avif" alt="{speaker.avl_speakers_id.fullname}" width="90px" height="90px">
+        <img src="https://fdnd-agency.directus.app/assets/{speaker.avl_speakers_id.profile_picture}?width=90&fit=cover&format=avif" alt="{speaker.avl_speakers_id.fullname}" width="90" height="90">
 
         <div>
           <h3>{speaker.avl_speakers_id.fullname}</h3>
@@ -96,7 +96,7 @@
         </div>
 
         <a href="/speakers/{speaker.avl_speakers_id.slug}">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
+          <svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="30" height="30" viewBox="0 0 48 48">
             <circle cx="24" cy="24" r="20" fill="var(--alt-text-color)" />
             <path d="M24,4C12.972,4,4,12.972,4,24s8.972,20,20,20s20-8.972,20-20S35.028,4,24,4z M25.5,33.5c0,0.828-0.672,1.5-1.5,1.5	s-1.5-0.672-1.5-1.5v-11c0-0.828,0.672-1.5,1.5-1.5s1.5,0.672,1.5,1.5V33.5z M24,18c-1.105,0-2-0.895-2-2c0-1.105,0.895-2,2-2	s2,0.895,2,2C26,17.105,25.105,18,24,18z"></path>
           </svg>
