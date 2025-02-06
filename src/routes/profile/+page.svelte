cd0f76eb053cc40a2fad646948804c85636e8e10
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-02-06 12:46:36 +0100

    desktop version almost done
    🔍 test: added a p element that contains a bio. Bio is part of the design its just a test to se how it would look like if it have one
    
    💄 style: added more mediaqueries to certain elements for the desktop layout
    
    -----

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -2,215 +2,261 @@
   import { WebinarOverview, FeaturedWebinar, ContouringOverview} from "$lib/index.js";
   export let data;
 
-  const user = data.user  
+  const user = data.user;
 
   console.log(user.profile_picture);
-  
-  let viewtransition = true
+
+  let viewtransition = true;
 </script>
 
 <main>
   <section>
     <h1>Profile</h1>
     <section>
-    <div>
-      {#if user.id && user.profile_picture}
-        <picture>
-          <img src="https://fdnd-agency.directus.app/assets/{user.profile_picture}?format=avif" alt="{user.profile_picture.title}" width="100" height="100">
-        </picture>
-        <div class="profile-info">
-          <p><i>{user.email}</i></p>
-          <h3>{user.fullname}</h3>
-          <span>{user.entitle}</span>
-        </div>
-      {:else}
-        <img src="/images/profilepic.png" alt="error" width="100" height="100">
-        <h4>Name Surname</h4>
-      {/if}
-    </div>
-    <div>
-      <button>
-        <span>EDIT</span>
-      </button>
-    </div>
+      <div>
+        {#if user.id && user.profile_picture}
+          <picture>
+            <img src="https://fdnd-agency.directus.app/assets/{user.profile_picture}?format=avif" alt={user.profile_picture.title} width="100" height="100"/>
+          </picture>
+          <div class="profile-info">
+            <p><i>{user.email}</i></p>
+            <h3>{user.fullname}</h3>
+            <span>{user.entitle}</span>
+            <p>Ik ben een enthousiaste student met een passie voor onderzoek en digitale innovatie. Mijn huidige studie richt zich op de impact en effectiviteit van webinars en contourings binnen educatieve en professionele omgevingen.
+
+              Mijn onderzoek draait om vragen als:
+              📌 Hoe kunnen webinars een interactievere leerervaring bieden?
+              📌 Welke technologieën verbeteren de nauwkeurigheid van contouring in medische of creatieve toepassingen?
+              📌 Wat zijn de best practices voor online educatie en visualisatie?
+              
+              Met een analytische blik en een nieuwsgierige mindset ben ik altijd op zoek naar nieuwe inzichten en methoden om deze onderwerpen verder te verkennen. 🚀</p>
+          </div>
+        {:else}
+          <img src="/images/profilepic.png" alt="error" width="100" height="100"/>
+          <h4>Name Surname</h4>
+        {/if}
+      </div>
+      <div>
+        <button alt="profile settings button">
+          <svg fill="currentColor" height="24" viewBox="0 -960 960 960" width="24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M480-160q-10 0-20-1t-20-3l-18 50q-3 10-12.5 15t-19.5 0l-102-41q-10-4-14.5-12t-1.5-18l19-52q-18-12-34-26t-30-30l-52 19q-10 4-18 0t-12-14l-41-102q-4-10 0-19.5t12-12.5l50-18q-3-10-3.5-20t-.5-20q0-10 .5-20t3.5-20l-50-18q-10-3-12-12.5t0-19.5l41-102q4-10 12-14t18 0l52 19q12-16 26-30t30-26l-19-52q-4-10 0-18t14-12l102-41q10-4 19.5 0t12.5 15l18 50q10-2 20-3t20-1q10 0 20 1t20 3l18-50q3-10 12.5-15t19.5 0l102 41q10 4 14.5 12t1.5 18l-19 52q18 12 34 26t30 30l52-19q10-4 18 0t12 14l41 102q4 10 0 19.5t-12 12.5l-50 18q3 10 3.5 20t.5 20q0 10-.5 20t-3.5 20l50 18q10 3 12 12.5t0 19.5l-41 102q-4 10-12 14t-18 0l-52-19q-12 16-26 30t-30 26l19 52q4 10 0 18t-14 12l-102 41q-10 4-19.5 0t-12.5-15l-18-50q-10 2-20 3t-20 1Zm0-160q66 0 113-47t47-113q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 66 47 113t113 47Z"/>
+          </svg>       
+          <span>EDIT</span>   
+        </button>
+      </div>
+    </section>
   </section>
+
+  <section>
+    <h3>Notifications</h3>
+    {#if data.webinars.length != 0}
+      <ul class="carrousel">
+        {#each data.webinars as webinar}
+          <li>
+            <WebinarOverview {...webinar} />
+          </li>
+        {/each}
+      </ul>
+    {:else}
+      <p>No webinars found for {data.category}.</p>
+    {/if}
   </section>
 
-<section>
-  <h3>Notifications</h3>
-  {#if data.webinars.length != 0}
+  <section>
+    <h3>History</h3>
+    {#if data.webinars.length != 0}
       <ul class="carrousel">
         {#each data.webinars as webinar}
           <li>
-            <WebinarOverview {...webinar}/>
+            <WebinarOverview {...webinar} />
           </li>
         {/each}
       </ul>
     {:else}
       <p>No webinars found for {data.category}.</p>
     {/if}
-</section>
+  </section>
 
-<section>
-  <h3>History</h3>
-  {#if data.webinars.length != 0}
+  <section>
+    <h3>Webinars</h3>
+    {#if data.webinars.length != 0}
       <ul class="carrousel">
         {#each data.webinars as webinar}
           <li>
-            <WebinarOverview {...webinar}/>
+            <WebinarOverview {...webinar} />
           </li>
         {/each}
       </ul>
     {:else}
       <p>No webinars found for {data.category}.</p>
     {/if}
-</section>
+  </section>
 
-<section>
-  <h3>Webinars</h3>
-  {#if data.webinars.length != 0}
+  <section>
+    {#if data.category != "all"}
+      <h3>Contouring results for {data.category}</h3>
+    {:else}
+      <h3>contourings</h3>
+    {/if}
+
+    {#if data.contourings.length != 0}
       <ul class="carrousel">
-        {#each data.webinars as webinar}
+        {#each data.contourings as contouring}
           <li>
-            <WebinarOverview {...webinar}/>
+            <ContouringOverview {...contouring} {viewtransition} />
           </li>
         {/each}
       </ul>
     {:else}
-      <p>No webinars found for {data.category}.</p>
+      <p>No contourings found for {data.category}.</p>
     {/if}
-</section>
-
-<section>
-  {#if data.category != 'all'}
-    <h3>Contouring results for {data.category}</h3>
-  {:else}
-    <h3>contourings</h3>
-  {/if}
-  
-  {#if data.contourings.length != 0}
-    <ul class="carrousel">
-      {#each data.contourings as contouring}
-      <li>
-        <ContouringOverview {...contouring} {viewtransition}/>
-      </li>
-      {/each}
-    </ul>
-  {:else}
-    <p>No contourings found for {data.category}.</p>
-  {/if}
-</section>
+  </section>
 </main>
 
 <style>
-
-  main > section{
+  main > section {
     margin: 4em 0;
   }
 
-  main section h1{
+  main section h1 {
     font-size: var(--font-size-2xl);
     text-transform: uppercase;
   }
 
-  main section h3{
+  main section h3 {
     font-size: var(--font-size-xl);
   }
 
   main section h1,
-  main section > h3{
+  main section > h3 {
     margin: 0.8em 0 0.5em 0;
   }
 
-  main section:nth-child(1) section{
+  main section:nth-child(1) section {
     display: flex;
     justify-content: space-between;
 
     @media (min-width: 600px) {
-      
-     display: block;
-  }
+      display: block;
+    }
   }
 
-  main section:nth-child(1) section > div{
+  main section:nth-child(1) section > div {
     display: flex;
-    @media (min-width: 500px){
+
+    @media (min-width: 500px) {
       width: 100%;
       justify-content: flex-end;
     }
-  }
-
-  main section:nth-child(1) section > div picture{
-    margin-right: 0.5rem;
-    width: 50%;  
-    height: auto;
-  }
-
-  main section:nth-child(1) section > div picture img{
-    border-radius: var(--border-radius-sm);
-    object-fit: cover;
 
     @media (min-width: 600px) {
-      width: 100%;
-      height: auto;
-      aspect-ratio: auto 1 / 1;
-      object-fit: contain;
-  }
-
-  @media (min-width: 600px) {
-      height: auto;
-      object-fit: cover;
-  }
-}
-
-  main section:nth-child(1) section div .profile-info{
-
-    @media (min-width: 600px){
-        align-items: center;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        width: 50%;
+      justify-content: flex-start;
+      margin-bottom: calc(1em - 5px);
     }
   }
 
-  main section:nth-child(1) section > div div i{
-    font-size: var(--font-size-sm);
+  main section:nth-child(1) section > div picture {
+
+    margin-right: 0.5rem;
+    height: auto;
+
+    @media (min-width: 1080px) {
+      margin-right: 10rem;
+    }
+  }
+
+  main section:nth-child(1) section > div picture img {
+    border-radius: var(--border-radius-sm);
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    aspect-ratio: 1 / 1; /* Dwing altijd een vierkante verhouding af */
+    max-width: 100px;
+    max-height: 100px;
+    display: block;
+
+    @media (min-width: 600px) {
+      max-width: 452px;
+      max-height: 452px;
+    }
+  }
+
+  main section:nth-child(1) section div .profile-info {
     
-    @media (min-width: 600px) {  
+    @media (min-width: 600px) {
+      align-items: flex-start;
+      display: flex;
+      flex-direction: column;
+      width: 50%;
+    }
+  }
+
+  main section:nth-child(1) section > div div p {
+    font-size: var(--font-size-sm);
+
+    @media (min-width: 600px) {
       font-size: var(--font-size-xl);
     }
   }
 
-  main section:nth-child(1) section > div div h3{
+  main section:nth-child(1) section > div div h3 {
     font-size: var(--font-size-lg);
+    
     @media (min-width: 600px) {
       font-size: var(--font-size-4xl);
     }
   }
 
-  main section:nth-child(1) section > div div span{
+  main section:nth-child(1) section > div div span {
     font-size: var(--font-size-md);
+    
     @media (min-width: 600px) {
       font-size: var(--font-size-3xl);
     }
   }
 
-  main section:nth-child(1) section > div button{
+  main section:nth-child(1) section > div div p:not(:has(i)){
+    display: none;
+
+    @media (min-width: 1080px) {
+      display: block;
+      margin-top: 1rem;
+    }
+  }
+
+  main section:nth-child(1) section > div button {
     background-color: var(--primary-color);
     border-radius: var(--border-radius-sm);
     border: none;
     align-self: flex-end;
     padding: var(--padding-button);
     cursor: pointer;
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 600px){
+      min-width: 78px;
+      min-height: 64px;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
-  main section:nth-child(1) section > div button span{
+  main section:nth-child(1) section > div button svg {
+    fill: var(--alt-text-color);
+    display: none;
+
+    @media (min-width: 600px){
+      display: block;
+    }
+  }
+
+  main section:nth-child(1) section > div button span {
     color: var(--alt-text-color);
     font-size: var(--font-size-sm);
   }
 
-  .carrousel{
+  .carrousel {
     display: flex;
     overflow-x: auto;
     scroll-snap-type: x mandatory;
@@ -221,21 +267,21 @@
     gap: 10px;
   }
 
-  .carrousel li{
+  .carrousel li {
     flex: 0 0 calc(100% - 20%);
     scroll-snap-align: start;
     border-radius: 10px;
     box-sizing: border-box;
-    @media only screen and (min-width: 500px) {
+    @media (min-width: 500px) {
       flex: 0 0 auto;
     }
   }
 
-  main > section:last-child  {
+  main > section:last-child {
     margin: 0 0 4em 0;
   }
 
-  main section:last-child ul li{
+  main section:last-child ul li {
     background-color: var(--background-category-color);
     padding: 1em 0.5em;
   }
