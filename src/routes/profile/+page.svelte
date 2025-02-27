a588404b30bd824cd95f4029c6d573fae0b45b4d
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-02-27 19:22:21 +0100

    added empty state
    🎇
    feature: first empty state is done and responsive as well
    
    next step creat a component that contains the same layout as every other empty state but with differenticons & text

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -99,7 +99,17 @@
         {/each}
       </ul>
     {:else}
-      <p>No webinars found.</p>
+    <ul class="carrousel">
+      <li>
+        <div class="empty-state">
+          <svg height="25px" width="25px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47.94 47.94" xml:space="preserve">
+            <title>star</title>
+            <path d="M26.285,2.486l5.407,10.956c0.376,0.762,1.103,1.29,1.944,1.412l12.091,1.757 c2.118,0.308,2.963,2.91,1.431,4.403l-8.749,8.528c-0.608,0.593-0.886,1.448-0.742,2.285l2.065,12.042 c0.362,2.109-1.852,3.717-3.746,2.722l-10.814-5.685c-0.752-0.395-1.651-0.395-2.403,0l-10.814,5.685 c-1.894,0.996-4.108-0.613-3.746-2.722l2.065-12.042c0.144-0.837-0.134-1.692-0.742-2.285l-8.749-8.528 c-1.532-1.494-0.687-4.096,1.431-4.403l12.091-1.757c0.841-0.122,1.568-0.65,1.944-1.412l5.407-10.956 C22.602,0.567,25.338,0.567,26.285,2.486z"/>
+          </svg>
+          <p>No favorites found, go <a href="/webinars">here</a></p>
+        </div>
+      </li>
+    </ul>
     {/if}
   </section>
 
@@ -293,4 +303,33 @@
     background-color: var(--background-category-color);
     padding: 1em 0.5em;
   }
+
+  .empty-state {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    aspect-ratio: 253 / 145;
+    border-radius: var(--border-radius-md);
+    background-color: var(--background-category-color); 
+
+    @media (min-width: 500px) {
+      width: var(--card-max-width);
+    }
+  }
+
+  .empty-state p {
+    color: var(--primary-color);
+    font-size: var(--font-size-sm);
+  }
+
+  .empty-state a {
+    color: var(--accent-color-1);
+  }
+
+  .empty-state path{
+    fill: var(--primary-color);
+    opacity: 0.7;
+  }
+
 </style>
