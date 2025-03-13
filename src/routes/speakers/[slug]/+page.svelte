f7a4275c628f95f9e8c68e23a00b33209a90817e
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-03-13 10:33:51 +0100

    fixed feedback from all the tests
    🔍 ♻️   refactor/test fixed all the issues with the label speaker and testen
    
    resolves: #57 #54 #60 #56 #47 #55 #45

--- src/routes/speakers/[slug]/+page.svelte
+++ src/routes/speakers/[slug]/+page.svelte
@@ -35,7 +35,7 @@
 </script>
 
 <main>
-  <button on:click={nav_back}>
+  <button on:click={nav_back} aria-label="back">
     <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M15 7L10 12L15 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
