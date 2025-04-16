c668f8385c045a1bfc094b67810f296a71db586c
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-04-16 13:49:24 +0200

    media nested in featured-webinar.svelte
    ♻️ refactor:  nested all the media quries in featured-webinar.svelte

--- src/lib/Blocks/featured-webinar.svelte
+++ src/lib/Blocks/featured-webinar.svelte
@@ -66,6 +66,11 @@
 
   article .container-image {
     position: relative;
+
+    @media (min-width: 882px) {
+      min-width: 50vw;
+      margin-right: 20px;
+    }
   }
 
   article .container-image img {
@@ -86,12 +91,24 @@
     right: 10px;
     border-radius: var(--border-radius-sm);
     z-index: 1;
+
+    @media (min-width: 882px) {
+      font-size: var(--font-size-lg);
+    }
   }
 
   article .speakers span {
     text-transform: uppercase;
     color: var(--primary-color);
     margin-block: 0.2rem;
+
+    @media (min-width: 882px) {
+      font-size: var(--font-size-md);
+    }
+
+    @media (min-width: 1180px) {
+      font-size: var(--font-size-lg);
+    }
   }
 
   .date {
@@ -100,6 +117,11 @@
     @media (min-width: 882px) {
       display: block;
       font-size: var(--font-size-md);
+    }
+
+    @media (min-width: 1180px) {
+      display: block;
+      font-size: var(--font-size-lg);
     }
   }
 
@@ -131,40 +153,11 @@
 
     @media (min-width: 882px) {
       font-size: var(--font-size-xl);
-    }
-  }
-
-  @media (min-width: 882px) {
-    
-   .container-image {
-      min-width: 50vw;
-      margin-right: 20px;
-    }
-
-    article .container-image .duration {
-      font-size: var(--font-size-lg);
-    }
-
-    p {
       padding: 8px 0;
     }
 
-    article .speakers span {
-      font-size: var(--font-size-md);
-    }
-  }
-  @media (min-width: 1180px) {
-    .featured-webinar-info p {
+    @media (min-width: 1180px) {
       font-size: var(--font-size-2xl);
-    }
-
-    article .speakers span {
-      font-size: var(--font-size-lg);
-    }
-    
-    .date {
-      display: block;
-      font-size: var(--font-size-lg);
     }
   }
 </style>
