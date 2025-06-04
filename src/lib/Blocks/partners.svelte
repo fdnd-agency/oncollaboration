e0e19c4894c6e14f3c494735e7e1621988d4bdac
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-06-04 11:03:23 +0200

    ver 1.0 parnters mobile
    🐛 📝   Style & fix: Removed some unnesacerry code like the console.log, Gave a grayscale and contrast to the img's to make them more vissible and lastly modified the time in the animation so that its easier to see the logos and also increased the gabetween them

--- src/lib/Blocks/partners.svelte
+++ src/lib/Blocks/partners.svelte
@@ -1,66 +1,84 @@
 <script>
-    import { onMount } from "svelte";
-    let { logos } = $props();
-
-    console.log("Logos length:", logos.length);
-
+  import { onMount } from "svelte";
+  let { logos } = $props();
 </script>
 
 <section>
-    <h2>Our partners</h2>
-    <div class="partner-container" style="
+  <h2>Our partners</h2>
+  <div
+    class="partner-container"
+    style="
     --width: 100px;
     --height: 74px;
     --quantity: {logos.length};
     ">
-        <div class="partner-logos">
-            {#each logos as logo, index}
-                <div class="logos" style="--position: {index + 1}"><img src="https://fdnd-agency.directus.app/assets/{logo.logo}" alt="{logo.name}" /></div>
-            {/each}
+    <div class="partner-logos">
+      {#each logos as logo, index}
+        <div class="logos" style="--position: {index + 1}">
+          <img src="https://fdnd-agency.directus.app/assets/{logo.logo}" alt={logo.name}/>
         </div>
+      {/each}
     </div>
-     <p>Our partners are essential to the success of Oncollaboration.
-Through their expertise, networks, and local knowledge, we can create lasting impact and improve cancer care together.</p>
+  </div>
+  <p>
+    Our partners are essential to the success of Oncollaboration.<br /><br />
+    Through their expertise, networks, and local knowledge, we can create lasting
+    impact and improve cancer care together.
+  </p>
 </section>
 
 <style>
-    .partner-container {
-     width: 100%;
-     height: var(--height);
-     overflow: hidden;
-     mask-image: linear-gradient(to right, transparent, #000 10% 90%, transparent);
+  h2 {
+    padding-left: 0.5em;
+    padding-bottom: 1em;
+  }
+
+  .partner-container {
+    width: 100%;
+    height: var(--height);
+    overflow: hidden;
+    mask-image: linear-gradient(
+      to right,
+      transparent,
+      #000 10% 90%,
+      transparent
+    );
+    margin: 1em 0;
+  }
+
+  .partner-container .partner-logos {
+    display: flex;
+    width: 100%;
+    min-width: calc(var(--width) * var(--quantity));
+    position: relative;
+  }
+
+  .partner-container .partner-logos .logos {
+    width: var(--width);
+    height: var(--height);
+    position: absolute;
+    left: 100%;
+    animation: autoslide 15s linear infinite;
+    animation-delay: calc((15s / var(--quantity)) * (var(--position) - 1));
+  }
+
+  .partner-container .partner-logos .logos img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    filter: grayscale(100%) contrast(0.2);
+  }
+
+  @keyframes autoslide {
+    from {
+      left: 100%;
     }
-
-    .partner-container .partner-logos {
-        display: flex;
-        width: 100%;
-        min-width: calc(var(--width) * var(--quantity));
-        position: relative;
+    to {
+      left: calc(var(--width) * -3); /* increasing the -3 will increase the gap between the logos */
     }
+  }
 
-    .partner-container .partner-logos .logos {
-        width: var(--width);
-        height: var(--height);
-        position: absolute;
-        left: 100%;
-        animation: autoslide 10s linear infinite;
-        animation-delay: calc((10s / var(--quantity)) * (var(--position) - 1) );
-    }
-
-    .partner-container .partner-logos .logos img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-    }
-
-    @keyframes autoslide {
-        from {
-            left: 100%;
-        }
-        to{
-            left: calc(var(--width) * -1);
-        }
-    }
-
-
+  p{
+    line-height: 1.5;
+  }
 </style>
