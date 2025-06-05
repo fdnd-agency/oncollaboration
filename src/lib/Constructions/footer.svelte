17aa6eb6a154ea1d4d46ed96d47fbf056aaedae4
J3SS3HVA <jesse.leeuwerink@hva.nl>
2025-06-05 16:03:08 +0200

    ver 1.0 footer mobile
    🎇 feature:  Created my first version for the footer.svelte mobile version

--- src/lib/Constructions/footer.svelte
+++ src/lib/Constructions/footer.svelte
@@ -1,106 +1,58 @@
 <footer>
-  <div>
-    <p>&copy; 2024 - Antoni van Leeuwenhoek</p>
-    <ul class="dot-divider">
-      <li>
-        <a href="/">Disclaimer</a>
-      </li>
-      <li>
-        <a href="/">Privacystatement</a>
-      </li>
-      <li>
-        <a href="/">Cookiestatement</a>
-      </li>
-    </ul>
-    <div class="footer-logos">
-      <img src="/images/AVL-Logo-white.svg" alt="Antoni van Leeuwenhoek">
-      <img src="/images/kemenkessardjito.png" alt="kemenkes ziekenhuis">
-    </div>
+  <p>&copy; 2024 - Antoni van Leeuwenhoek</p>
+  <ul class="dot-divider">
+    <li>
+      <a href="/">Disclaimer</a>
+    </li>
+    <li>
+      <a href="/">Privacystatement</a>
+    </li>
+    <li>
+      <a href="/">Cookiestatement</a>
+    </li>
+  </ul>
+  <div class="footer-logos">
+    <img src="images/LogoOncollaboration.png" width="40" height="45" alt="logo" class="s-lCPQtr6yAWsI">
+    <img src="images/Kemenkes2.png" width="40" height="45" alt="Logo" class="s-lCPQtr6yAWsI">
   </div>
 </footer>
 
 <style>
   footer {
-    display: none;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding-top: 0.6rem;
+    font-size: 11px;
+    background-color: var(--primary-color);
 
     @media (min-width: 1080px) {
-      display: flex;
       position: sticky;
       bottom: 0;
-      align-items: center;
-      justify-content: space-between;
-      background-color: var(--primary-color);
-      padding: 1rem 0rem;
-      color: var(--alt-text-color);
     }
+  }
+
+  footer p {
+    color: var(--text-color-light);
   }
 
   footer ul {
-    @media (min-width: 1080px) {
-      display: flex;
-    }
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
   }
 
   footer li {
-    @media (min-width: 1080px) {
-      padding: 0 0.8rem;
-      position: relative;
-    }
-  }
-
-  footer div  {
-    @media (min-width: 1080px) {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      margin: 0 auto;
-      width: 85vw;
-    }
-  }
-
-  footer img {
-    @media (min-width: 1080px) {
-      height: 3.2rem;
-    }
-  }
-
-  .footer-logos {
-    @media (min-width: 1080px) {
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      gap: 1rem; 
-    }
+    list-style: none;
   }
 
   footer a {
-    @media (min-width: 1080px) {
-      font-weight: normal;
-      color: var(--alt-text-color);
-      text-decoration: none;
-    }
+    color: var(--text-color-light);
   }
 
   footer a:hover {
-    @media (min-width: 1080px) {
-      text-decoration: underline;
-    }
+    text-decoration: underline;
   }
-
-  .dot-divider li::after{
-    @media (min-width: 1080px) {
-      content: '•';
-      position: absolute;
-      right: -0.2rem;
-      top: 50%;
-      transform: translateY(-50%);
-    }
-  }
-
-  .dot-divider li:last-child::after {
-    @media (min-width: 1080px) {
-      content: '';
-    }
-  }
-
 </style>
