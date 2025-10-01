7375f622c6dacb37dc9a8aee3cf6c90d5a4a15c4
Sidopjescherm <hermussidney@gmail.com>
2025-10-01 12:03:24 +0200

    fix #233 & #237
    We hebben de overige mappen verwijderd die wij niet hebben gemaakt en nog niet mee bezig zijn. Daarom is er zoveel verwijderd. De data die ingeladen wordt op pagina's weten wij zelf ook beter te kunnen en daarom gaan die hier nu ook weg en opnieuw zullen verwerkt worden
    
    De countourings route heb ik nu ook verwijderd omdat wij denken het niet nodig vinden om die route nu al te hebben.

--- src/lib/Pages/Contourings/Bricks/contouring-header.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Contourings</h1>
-
-<style>
-  h1 {
-    font-size: var(--font-size-sm);
-    opacity: 0;
-  }
-</style>
--- src/lib/Pages/Webinars/Bricks/webinar-header.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Webinars</h1>
-
-<style>
-  h1 {
-    font-size: var(--font-size-sm);
-    opacity: 0;
-  }
-</style>
