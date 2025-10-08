dd12e7e78404721991ff0c2e51946b286158d487
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-08 11:28:10 +0200

    Update in code, try & catch toegevoegd aan de data die opgehaald wordt.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,25 @@
+
 export async function load({ url }) {
-    const homepageAbout = await fetch ('https://fdnd-agency.directus.app/items/avl_content/3');
-    const homepageAboutData = await homepageAbout.json();
 
+    // trys to load the data and send it
 
-    return {about: homepageAboutData.data}
+    try {
+        const homepageAbout = await fetch('https://fdnd-agency.directus.app/items/avl_content/3');
+
+        if (!homepageAbout.ok) {
+            throw new Error(`HTTP error! status: ${homepageAbout.status}`);
+        }
+
+        const homepageAboutData = await homepageAbout.json();
+        return { about: homepageAboutData.data, error: null };
+
+    // if there is error handles it and gives error message in return to the user
+
+    } catch (error) {
+
+        return {
+            about: null,
+            error: "Sorry, error with loading the data please try again."
+        };
+    }
 }
