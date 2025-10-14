f4aeb780508090904a0c3ed1b9346de82abc7e5a
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-14 12:51:09 +0200

    mediaquerys for the webinars and contourings, and changed one small grid thing for the about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
     const infocontourings = data.contourings;
 </script>
 
-<section class="homepage-about home-mobile-styling">
+<article class="homepage-about home-mobile-styling">
     <h1 class="header-about">{infoabout.heading}</h1>
     <p class="info-about">{infoabout.text}</p>
-    <a class="button-style" href="/about">More about oncollaboration<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <a class="button-style button-homepage-about" href="/about">More about oncollaboration<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
         </svg>
     </a>
@@ -19,24 +19,25 @@
         <source srcset="/images/doctors.webp" type="image/webp">
         <img class="img-about" src="/images/doctors.png" alt="group of doctors" loading="lazy">
      </picture>
-</section>
+</article>
 
-<section class="homepage-webinars home-mobile-styling">
-    <h2>{infowebinars.heading}</h2>
-    <p>{infowebinars.text}</p>
-    <a class="button-style" href="/webinars">More about webinars<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
-        </svg>
-    </a>
-</section>
-
-<section class="homepage-contourings home-mobile-styling">
-    <h2>{infocontourings.heading}</h2>
-    <p>{infocontourings.text}</p>
-    <a class="button-style" href="/contourings">More about contourings<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
-        </svg>
-    </a>
+<section class="homepage-webinars-contourings">
+    <article class="homepage-webinars home-mobile-styling">
+        <h2>{infowebinars.heading}</h2>
+        <p>{infowebinars.text}</p>
+        <a class="button-style" href="/webinars">More about webinars<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+           <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
+           </svg>
+        </a>
+    </article>
+    <article class="homepage-contourings home-mobile-styling">
+        <h2>{infocontourings.heading}</h2>
+        <p>{infocontourings.text}</p>
+        <a class="button-style" href="/contourings">More about contourings<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
+            </svg>
+        </a>
+    </article>
 </section>
 
 
@@ -55,21 +56,24 @@
 
     .homepage-about {
         padding-top: 4em;
+        padding-bottom: 2.5em;
 
         @media ( min-width: 56.25em ) {
             display: grid;
             grid-template-columns: 0.8fr 1fr;
-            grid-template-rows: 14.5em 18.75em 3.125em;
+            grid-template-rows: 18.5em 13.75em 3.125em;
             column-gap: 2em;
         }
 
         @media ( min-width: 62.5em ) {
-            grid-template-rows: 10.375em 15.625em 3.125em;
+            grid-template-rows: 12.375em 12.625em 4.125em;
+
         }
 
         @media ( min-width: 75em ) {
             grid-template-columns: 30em 37.5em;
             justify-content: center;
+            column-gap: 6em;
         }
     }
 
@@ -90,6 +94,17 @@
         }
     }
 
+    .button-homepage-about {
+        margin-top: 2.25em;
+        margin-bottom: 2.5em;
+        
+        @media ( min-width: 56.25em ) {
+            grid-column: 1/2;
+            align-self: self-end;
+            margin: 0;
+        }
+    }
+
     .button-style {
         display: flex;
         justify-content: center;
@@ -101,14 +116,9 @@
         text-decoration: none;
         gap: 0.25em;
         border-radius: 0.5em;
-        margin-top: 2.25em;
-        margin-bottom: 2.5em;
 
         @media ( min-width: 56.25em ) {
-            grid-column: 1/2;
             max-width: 20.625em;
-            margin-top: 0;
-            margin-bottom: 0;
         }
     }
 
@@ -118,13 +128,37 @@
 
         @media ( min-width: 56.25em ) {
             grid-column: 2/3;
-            grid-row: 1/3;
-            margin-top: 3.5em;
+            grid-row: 2/3;
+            align-self: center;
         }
     }
 
-    .homepage-webinars {
-        background-color: var(--primary-color-blue-light-1);
+    .homepage-webinars-contourings {
+
+        @media ( min-width: 56.25em ) {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: var(--primary-color-blue-dark-2);
+            gap: 2em;
+        }
+
+        @media ( min-width: 75em ) {
+            gap: 7em;
+        }
     }
+
+    .homepage-webinars, .homepage-contourings  {
+        padding-bottom: 2.5em;
+        background-color: var(--primary-color-blue-light-1);
+
+        @media ( min-width: 56.25em ) {
+            max-width: 27.25em;
+            padding-inline: 3em;
+            border-radius: 1em;
+        }
+    }
+        
+
 
 </style>    
