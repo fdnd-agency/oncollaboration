15baa246f87ff887cacafbe1bb8e329a275a65c1
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-14 11:24:27 +0200

    contouringsdata & webinarsdata in html

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,8 @@ export async function load({ url }) {
     try {
 
         const homepageAbout = await fetch('https://fdnd-agency.directus.app/items/avl_content/3');
-        const homepageWebinars = await fetch('https://fdnd-agency.directus.app/items/avl_content?filter[id][_in]=6,7');
+        const homepageWebinars = await fetch('https://fdnd-agency.directus.app/items/avl_content/6');
+        const homepageContourings = await fetch('https://fdnd-agency.directus.app/items/avl_content/7')
 
         if (!homepageAbout.ok) {
             throw new Error(`HTTP error! status: ${homepageAbout.status}`);
@@ -11,19 +12,26 @@ export async function load({ url }) {
         if (!homepageWebinars.ok) {
             throw new Error(`HTTP error! status: ${homepageWebinars.status}`);
         }
+
+        if (!homepageContourings.ok) {
+            throw new Error(`HTTP error! status: ${homepageContourings.status}`);
+        }
         
         const homepageAboutData = await homepageAbout.json();
         const homepageWebinarsData = await homepageWebinars.json();
+        const homepageContouringsData = await homepageContourings.json();
 
         return {
             about: homepageAboutData.data,
             webinars: homepageWebinarsData.data,
+            contourings: homepageContouringsData.data,
             error: null
         };
     } catch (error) {
         return {
             about: null,
             webinars: null,
+            contourings: null,
             error: error.message || "Sorry, error with loading the data. Please try again."
         };
     }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 
     const infoabout = data.about;
     const infowebinars = data.webinars;
+    const infocontourings = data.contourings;
 </script>
 
 <section class="homepage-about">
@@ -20,8 +21,22 @@
      </picture>
 </section>
 
-<section class="homepage-contourings-webinars">
+<section class="homepage-webinars">
+    <h2>{infowebinars.heading}</h2>
+    <p>{infowebinars.text}</p>
+    <a class="button-style" href="/webinars">More about webinars<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
+        </svg>
+    </a>
+</section>
 
+<section class="homepage-contourings">
+    <h2>{infocontourings.heading}</h2>
+    <p>{infocontourings.text}</p>
+    <a class="button-style" href="/contourings">More about contourings<svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.864 10.4C11.024 10.0587 11.1787 9.76 11.328 9.504C11.488 9.248 11.6427 9.03467 11.792 8.864H2.464V8.192H11.792C11.6427 8.01067 11.488 7.792 11.328 7.536C11.1787 7.28 11.024 6.98667 10.864 6.656H11.424C12.096 7.43467 12.8 8.01067 13.536 8.384V8.672C12.8 9.03467 12.096 9.61067 11.424 10.4H10.864Z" fill="#00193F"/>
+        </svg>
+    </a>
 </section>
 
 
