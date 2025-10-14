06a57c4819e40b5a75c1c27ea61777c70389398f
KyanTG <kyan.gijtenbeek@hva.nl>
2025-10-14 12:00:02 +0200

    server for webinars and contourings

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,25 +1,38 @@
-
 export async function load({ url }) {
-
-    // trys to load the data and send it
-
     try {
+
         const homepageAbout = await fetch('https://fdnd-agency.directus.app/items/avl_content/3');
+        const homepageWebinars = await fetch('https://fdnd-agency.directus.app/items/avl_content/6');
+        const homepageContourings = await fetch('https://fdnd-agency.directus.app/items/avl_content/7')
 
         if (!homepageAbout.ok) {
             throw new Error(`HTTP error! status: ${homepageAbout.status}`);
         }
 
+        if (!homepageWebinars.ok) {
+            throw new Error(`HTTP error! status: ${homepageWebinars.status}`);
+        }
+
+        if (!homepageContourings.ok) {
+            throw new Error(`HTTP error! status: ${homepageContourings.status}`);
+        }
+        
         const homepageAboutData = await homepageAbout.json();
-        return { about: homepageAboutData.data, error: null };
-
-    // if there is error handles it and gives error message in return to the user
-
-    } catch (error) {
+        const homepageWebinarsData = await homepageWebinars.json();
+        const homepageContouringsData = await homepageContourings.json();
 
         return {
+            about: homepageAboutData.data,
+            webinars: homepageWebinarsData.data,
+            contourings: homepageContouringsData.data,
+            error: null
+        };
+    } catch (error) {
+        return {
             about: null,
-            error: "Sorry, error with loading the data please try again."
+            webinars: null,
+            contourings: null,
+            error: error.message || "Sorry, error with loading the data. Please try again."
         };
     }
 }
