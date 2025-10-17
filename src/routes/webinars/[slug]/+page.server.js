1061f572eaa57df1537f96310cca7081c80c20c7
Sidopjescherm <hermussidney@gmail.com>
2025-10-17 14:12:40 +0200

    #270 Filteren zodat de categorieën er ook zijn
    Na het bekijken van het project dat vorige jaar draaide heb ik de fetch aangepast zodat we de velden fetchen die we nodig hebben.

--- src/routes/webinars/[slug]/+page.server.js
+++ src/routes/webinars/[slug]/+page.server.js
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit'
 
 export async function load({params}) {
-        const webinarURL = await fetch (`https://fdnd-agency.directus.app/items/avl_webinars/?filter[slug][_eq]=${params.slug}`)
+        const webinarURL = await fetch (`https://fdnd-agency.directus.app/items/avl_webinars?fields=slug,thumbnail.id,thumbnail.title,duration,title,video,speakers.avl_speakers_id.fullname,categories.avl_categories_id.name,date&filter[slug][_eq]=${params.slug}`)
         // const categoriesURL = await fetch(`https://fdnd-agency.directus.app/items/avl_categories`)
         // console.log(webinarURL)
         // console.log(categoriesURL)
