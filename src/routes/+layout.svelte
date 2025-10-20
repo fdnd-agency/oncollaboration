14310554242c15a7c422d6bf76e9cc7bbf1d74d0
nadiachaja <nadiagaja13@gmail.com>
2025-10-20 15:16:13 +0200

    #303 begin html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script>
-    import "$lib/global.css"
+    import "$lib/global.css";
     let { children } = $props();
 </script>
+
+<header>
+    <nav>
+        <img src="" alt="" />
+        <img src="" alt="" />
+
+        <ul>
+            <li><a href={`/webinars`}>Webinars</a></li>
+            <li><a href={`/contourings`}>Contourings</a></li>
+            <li><a href={`/speakers`}>Speakers</a></li>
+            <li><a href={`/profile`}>Profile</a></li>
+        </ul>
+
+        <select id="language" name="language">
+            <option value="language">Language</option>
+            <option value="English">English</option>
+            <option value="Indonesian">Indonesian</option>
+        </select>
+    </nav>
+</header>
 
 {@render children?.()}
