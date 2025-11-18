29c32ef6aa56637813a850b5e4bf0d26317addd5
nadiachaja <nadiagaja13@gmail.com>
2025-11-18 15:39:05 +0100

    #321 bestanden in index.js

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+
+//Atoms
+export { default as ButtonFilteren } from './atoms/button-filteren.svelte' 
+export { default as ButtonReset } from './atoms/button-reset.svelte' 
+export { default as SelectAlfabetisch } from './atoms/select-alfabetisch.svelte' 
+export { default as SelectRecent } from './atoms/select-recent.svelte' 
+export { default as SelectCategory } from './atoms/select-category.svelte' 
+
+
+//Molecules
+export { default as SearchBar } from './molecules/searchbar.svelte'
