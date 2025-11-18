e8f529272478e3120e523bd60c431206ecd1949a
nadiachaja <nadiagaja13@gmail.com>
2025-11-18 15:40:28 +0100

    #321 oude code weg

--- src/lib/molecules/searchbar.svelte
+++
@@ -1,8 +0,0 @@
-<label class="label-search" for="webinarSearch"
-    >Type here a webinar you are looking for</label
->
-
-<div class="searchbar">
-    <input type="search" id="webinarSearch" placeholder="Search..." />
-    <button class="img-search"><img src={search} alt="" /></button>
-</div>
