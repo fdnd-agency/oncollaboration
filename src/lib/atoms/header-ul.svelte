4d73235f5b77c0aaa3920299b369104705f4a903
KyanTG <kyan.gijtenbeek@hva.nl>
2025-11-24 14:35:33 +0100

    fixed atom header ul
    #317

--- src/lib/atoms/header-ul.svelte
+++ src/lib/atoms/header-ul.svelte
@@ -13,47 +13,14 @@
     </style>
 </svelte:head>
 
-<nav class="nav-header" id="menu">
-    <a href="#" class="close"><img src={close} alt="close menu" height="50" width="50" /></a>
-    <ul>
-        <li><a class="webinars" href={`/webinars`}>Webinars</a></li>
-        <li><a class="contourings" href={`/contourings`}>Contourings</a></li>
-        <li><a class="speakers" href={`/speakers`}>Speakers</a></li>
-        <li><a class="profile" href={`/profile`}>Profile</a></li>
-    </ul>
-</nav>
+<ul>
+    <li><a class="webinars" href={`/webinars`}>Webinars</a></li>
+    <li><a class="contourings" href={`/contourings`}>Contourings</a></li>
+    <li><a class="speakers" href={`/speakers`}>Speakers</a></li>
+    <li><a class="profile" href={`/profile`}>Profile</a></li>
+</ul>
 
-<style>
-        .nav-header {
-        position: fixed;
-        top: 0;
-        right: -100%;
-        width: 100%;
-        background: var(--primary-color-blue-dark-2);
-        transition: right 0.3s ease;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        padding: 1em;
-
-        @media (min-width: 600px) {
-            padding: 1em 0em 0em 0em;
-            flex-direction: unset;
-        }
-
-        @media (min-width: 900px) {
-            position: static;
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-end;
-            gap: 2em;
-            padding: 0;
-            right: auto;
-        }
-    }
-
-    
+<style>  
     ul {
         list-style-type: none;
         display: flex;
