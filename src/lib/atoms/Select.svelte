dd11e5543e47e817ead8e688bbad120d80155706
nadiachaja <nadiagaja13@gmail.com>
2025-11-25 14:58:22 +0100

    fix: #324 safari probleem bij de selects opgelost

--- src/lib/atoms/Select.svelte
+++ src/lib/atoms/Select.svelte
@@ -1,33 +1,36 @@
 <script>
-    let {
-        children,
-        name = '',
-        ...restProps
-    } = $props()
+    let { children, name = "", ...restProps } = $props();
+    import arrowDownSvg from "$lib/assets/arrow-down.svg";
 </script>
 
-
-<select name={name} {...restProps}>
+<select {name} {...restProps} style="--arrow: url({arrowDownSvg})">
     {@render children?.()}
 </select>
 
 <style>
- select {
+    select {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
         padding: 0.5em;
         border-radius: var(--border-radius-small);
         font-size: var(--font-size-small);
         font-family: var(--primary-font-family);
+        background-image: var(--arrow);
+        background-repeat: no-repeat;
+        background-position: right 0.5em center;
+        background-size: 1em;
     }
 
     @container (min-width: 700px) {
-    #category {
-        grid-column: 2 / 1; 
+        #category {
+            grid-column: 2 / 1;
+        }
     }
-}
 
-@container (min-width: 700px) {
-    #recent {
-        grid-column: 2 / 2;
+    @container (min-width: 700px) {
+        #recent {
+            grid-column: 2 / 2;
+        }
     }
-}
 </style>
