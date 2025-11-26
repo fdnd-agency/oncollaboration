150d87e9827bec36f0f33c7c4af99a74c775e90d
KyanTG <kyan.gijtenbeek@hva.nl>
2025-11-26 10:24:57 +0100

    header language

---
+++ src/lib/atoms/header-language.svelte
@@ -0,0 +1,37 @@
+<script>
+    import "$lib/global.css";
+</script>
+
+<svelte:head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: var(--primary-font-family);
+        }
+    </style>
+</svelte:head>
+
+<select id="language" name="language">
+    <option value="language">Language</option>
+    <option value="English">English</option>
+    <option value="Indonesian">Indonesian</option>
+</select>
+
+<style>
+    select {
+        background-color: var(--primary-color-aqua-dark-3);
+        border: var(--primary-color-aqua-dark-3);
+        border-radius: var(--border-radius-small);
+        font-size: var(--font-size-small);
+        font-family: var(--primary-font-family);
+        width: fit-content;
+        height: fit-content;
+        padding: 0.5em;
+
+        @media (min-width: 500px) {
+            margin: 0em 1em 0em 0em;
+        }
+    }
+</style>
