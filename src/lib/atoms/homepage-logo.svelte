9205b7f2f260f4d1ce37020975ef68c9d85a878b
KyanTG <kyan.gijtenbeek@hva.nl>
2025-12-02 11:32:05 +0100

    refactor: logo carrousel atom for component

---
+++ src/lib/atoms/homepage-logo.svelte
@@ -0,0 +1,34 @@
+<script>
+    let { data } = $props();
+    import "$lib/global.css";
+    const infologos = data.logos;
+</script>
+
+<svelte:head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: var(--primary-font-family);
+        }
+    </style>
+</svelte:head>
+
+{#each infologos as logo}
+    <picture class="logos-partnerships">
+        <source srcSet={`https://fdnd-agency.directus.app/assets/${logo.logo}?format=avif`} type="image/avif"/>
+        <source srcSet={`https://fdnd-agency.directus.app/assets/${logo.logo}?format=webp`} type="image/webp"/>
+        <img src={`https://fdnd-agency.directus.app/assets/${logo.logo}`} alt={logo.name} loading="lazy"/>
+    </picture>
+{/each}
+
+<style>
+    .logos-partnerships {
+        display: flex;
+        align-items: center;
+        object-fit: contain;
+        flex-shrink: 0; 
+        margin-left: 2rem;
+    }
+</style>
