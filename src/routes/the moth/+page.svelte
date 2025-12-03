7a4a1cf7c43b56c1621cc893a949a1ee1847bc88
Sidopjescherm <hermussidney@gmail.com>
2025-12-03 15:48:55 +0100

    #350 Pagina in Svelte
    Zoals ik heb gedaan in #346 heb ik het ontwerp overgemaakt naar HTML elementen.

--- src/routes/the moth/+page.svelte
+++ src/routes/the moth/+page.svelte
@@ -2,3 +2,22 @@
     import { onMount } from "svelte";
     import gsap from "gsap/dist/gsap";
 </script>
+
+<section>
+    <section>
+        <h1>MOTH CORE</h1>
+        <picture></picture>
+    </section>
+
+    <section>
+        <h2>EXIT</h2>
+        <picture></picture>
+        <h3>COCOON</h3>
+    </section>
+
+    <section>
+        <h2>FLY</h2>
+        <picture>
+        </picture>
+    </section>
+</section>
