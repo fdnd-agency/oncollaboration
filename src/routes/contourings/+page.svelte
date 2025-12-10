fdd7d6a41c554a2e215dfff553b319e72829c73a
nadiachaja <nadiagaja13@gmail.com>
2025-12-10 16:40:52 +0100

    refactor: maan png voor sprite

--- src/routes/contourings/+page.svelte
+++ src/routes/contourings/+page.svelte
@@ -1,14 +1,7 @@
 <script>
     import backgroundPNG from "$lib/assets/landschap.png";
     import foregroundPNG from "$lib/assets/voorgrond.png";
-    import moon1PNG from "$lib/assets/moon-1.png";
-    import moon2PNG from "$lib/assets/moon-2.png";
-    import moon3PNG from "$lib/assets/moon-3.png";
-    import moon4PNG from "$lib/assets/moon-4.png";
-    import moon5PNG from "$lib/assets/moon-5.png";
-    import moon6PNG from "$lib/assets/moon-6.png";
-    import moon7PNG from "$lib/assets/moon-7.png";
-    import moon8PNG from "$lib/assets/moon-8.png";
+    import mooncyclusPNG from "$lib/assets/mooncyclus.png";
 
 
 </script>
