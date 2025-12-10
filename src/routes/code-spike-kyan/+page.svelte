823e2aaa02b1337b78a18a913068d5fabedde2f3
KyanTG <kyan.gijtenbeek@hva.nl>
2025-12-10 12:46:14 +0100

    fix: added al svg imports

--- src/routes/code-spike-kyan/+page.svelte
+++ src/routes/code-spike-kyan/+page.svelte
@@ -1,3 +1,15 @@
 <script>
-    
+    import la1 from "$lib/assets/line-art-1.svg";
+    import la2 from "$lib/assets/line-art-2.svg";
+    import la3 from "$lib/assets/line-art-3.svg";
+    import la4 from "$lib/assets/line-art-4.svg";
+    import la5 from "$lib/assets/line-art-5.svg";
+    import la6 from "$lib/assets/line-art-6.svg";
+    import la7 from "$lib/assets/line-art-7.svg";
+    import la8 from "$lib/assets/line-art-8.svg";
+    import la9 from "$lib/assets/line-art-9.svg";
+    import la10 from "$lib/assets/line-art-10.svg";
+    import la11 from "$lib/assets/line-art-11.svg";
+    import la12 from "$lib/assets/line-art-12.svg";
+    import la13 from "$lib/assets/line-art-13.svg";
 </script>
