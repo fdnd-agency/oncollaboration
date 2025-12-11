4483276f5995fdd4a43e8fdb04e62f115a5a8d5e
KyanTG <kyan.gijtenbeek@hva.nl>
2025-12-11 11:59:54 +0100

    feature: fixing heading text with spans so i can animate it

--- src/routes/code-spike-kyan/+page.svelte
+++ src/routes/code-spike-kyan/+page.svelte
@@ -15,7 +15,32 @@
     import la13 from "$lib/assets/line-art-13.svg";
 </script>
 
-<h1>Disastrous line-art scroll driven animation</h1>
+
+<svelte:head> 
+    <style>
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            overflow-x: hidden;
+        }
+        
+    </style>
+</svelte:head>
+
+
+<section>
+    <span>D</span><span>i</span><span>s</span><span>a</span><span>s</span><span>t</span><span>r</span><span>o</span><span>u</span><span>s</span>
+    <span class="space"></span>
+    <span>l</span><span>i</span><span>n</span><span>e</span><span>-</span><span>a</span><span>r</span><span>t</span>
+    <span class="space"></span>
+    <span>s</span><span>c</span><span>r</span><span>o</span><span>l</span><span>l</span>
+    <span class="space"></span>
+    <span>d</span><span>r</span><span>i</span><span>v</span><span>e</span><span>n</span>
+    <span class="space"></span>
+    <span>a</span><span>n</span><span>i</span><span>m</span><span>a</span><span>t</span><span>i</span><span>o</span><span>n</span>
+</section>
 
 <section>
     <img src="{la1}" alt="line art">
@@ -30,33 +55,125 @@
     <img src="{la10}" alt="line art">
     <img src="{la11}" alt="line art">
     <img src="{la12}" alt="line art">
-    <img src="{la13}" alt="line art">
+    <img src="{la13}" alt="line art"> 
 </section>
 
 <style>
 
-    h1 {
+    /* h1 {
         font-size: 4rem;
         font-weight: 700;
         text-align: center;
         margin-top: 10rem;
         margin-bottom: 30rem;
+    } */
+
+    span {
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: 700;
+        font-size: 3rem;
     }
 
-    section {
+    .space {
+        margin-left: 0.8rem;
+    }
+
+    section:nth-of-type(1) {
+        display: flex;
+        justify-content: center;    
+    }
+    
+    section:nth-of-type(2) {
         display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(20, 1fr);
-        height: 600vh;
+        grid-template-columns: repeat(6, 250px);
+        grid-template-rows: repeat(14, 1fr);
+        height: 300vh;
+        width: 100vw;
+        padding-inline: 2rem;
     }
 
     img {
         animation-timeline: view();
+        width: calc(sibling-index() * 50px);
+        height: calc(sibling-index() * 50px);
+        min-height: 200px;
+        min-width: 200px;
+
     }
 
     img:nth-of-type(1) {
-        grid-column: 1/2;
+        grid-column: 3/4;
+        grid-row: 1/2;
+    }
+
+    img:nth-of-type(2) {
+        grid-column: 5/7;
         grid-row: 2/3;
+    }
+
+    img:nth-of-type(3) {
+        grid-column: 1/2;
+        grid-row: 3/4;
+    }
+
+    img:nth-of-type(4) {
+        grid-column: 3/4;
+        grid-row: 4/5;
+    }
+
+    img:nth-of-type(5) {
+        grid-column: 5/7;
+        grid-row: 5/6;
+    }
+
+    img:nth-of-type(6) {
+        grid-column: 2/3;
+        grid-row: 6/7;
+    }
+
+    img:nth-of-type(7) {
+        grid-column: 5/6;
+        grid-row: 7/8;
+    }
+
+    img:nth-of-type(8) {
+        grid-column: 1/3;
+        grid-row: 8/9;
+    }
+
+    img:nth-of-type(9) {
+        grid-column: 3/4;
+        grid-row: 9/10;
+    }
+
+    img:nth-of-type(10) {
+        grid-column: 4/6;
+        grid-row: 10/11;
+    }
+
+    img:nth-of-type(11) {
+        grid-column: 1/3;
+        grid-row: 11/12;
+    }
+
+    img:nth-of-type(12) {
+        grid-column: 4/6;
+        grid-row: 12/13;
+    }
+
+    img:nth-of-type(13) {
+        grid-column: 2/5;
+        grid-row: 13/14;
+    }
+
+    @keyframes FunnyLetters {
+        from {
+            rotate: 0deg;
+        }
+
+        to {
+            rotate: 1turn;
+        }
     }
 </style>
 
