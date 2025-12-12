062e54548ba54cc9d4691368e84e13761100f7bd
nadiachaja <nadiagaja13@gmail.com>
2025-12-12 14:19:46 +0100

    feat: #367 scoop toegevoegd ster

--- src/routes/contourings/+page.svelte
+++ src/routes/contourings/+page.svelte
@@ -7,15 +7,22 @@
 </script>
 
 <div class="image-stack">
-<img class="bg-img" src={backgroundPNG} alt="">
+  <img class="bg-img" src={backgroundPNG} alt="">
 
-<h1>Moon</h1>
+  <h1>Moon</h1>
 
-<div class="cirkel">
-<div class="moon" style="--moons: url({mooncyclusPNG})"></div>
-</div>
+  <div class="scoop1"></div>
+  <div class="scoop2"></div>
+  <div class="scoop3"></div>
+  <div class="scoop4"></div>
+  <div class="scoop5"></div>
+  <div class="scoop6"></div>
 
-<img class="fore-img" src={foregroundPNG} alt="">
+  <div class="cirkel">
+    <div class="moon" style="--moons: url({mooncyclusPNG})"></div>
+  </div>
+
+  <img class="fore-img" src={foregroundPNG} alt="">
 </div>
 
 <style>
@@ -37,6 +44,85 @@ h1{
   z-index: 4;
 }
 
+.scoop1{
+  background-color:lightyellow;
+  width:2em;
+  height:2em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:4em;
+  right:4em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+.scoop2{
+  background-color:lightyellow;
+  width:1em;
+  height:1em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:2em;
+  right:5em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+.scoop3{
+  background-color:lightyellow;
+  width:3em;
+  height:3em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:2em;
+  right:5em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+.scoop4{
+  background-color:lightyellow;
+  width:1em;
+  height:1em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:8em;
+  left:45em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+.scoop5{
+  background-color:lightyellow;
+  width:1.5em;
+  height:1.5em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:7em;
+  left:43em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+.scoop6{
+  background-color:lightyellow;
+  width:1em;
+  height:1em;
+  border-radius: 50px;
+  corner-shape: scoop;
+  position: absolute;
+  top:2em;
+  right:30em;
+  z-index: 6;
+  animation: star 1s infinite;
+}
+
+
 .bg-img{
   z-index: 1;
 }
@@ -52,6 +138,11 @@ h1{
   width: 100%;   
   height: 100vh;   
   object-fit: cover;
+}
+
+.cirkel{
+  animation: cirkel 5s ;
+  border: 1px solid red;
 }
 
 .moon {
@@ -74,6 +165,12 @@ h1{
 
 }
 
+@keyframes star {
+  0%   { transform: rotate(-10deg); }
+  50%  { transform: rotate(10deg); }
+  100% { transform: rotate(-10deg); }
+}
+
 @keyframes moon {
   from { background-position-x: 0; }
   to   { background-position-x: -1192px; }
@@ -83,6 +180,7 @@ h1{
     0% { transform: translate(-50%, -50%) rotate(0deg) translateY(-100px); }
     100% { transform: translate(-50%, -50%) rotate(360deg) translateY(-100px); }
   }
+
 
 </style>
 
