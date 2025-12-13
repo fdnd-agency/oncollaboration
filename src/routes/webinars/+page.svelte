1dbd485910ccccbfe058238a368c22b27bb4e105
nadiachaja <nadiagaja13@gmail.com>
2025-12-13 12:27:15 +0100

    refactor: #338 media queries/ gap aangepast en padding toegevoegd

--- src/routes/webinars/+page.svelte
+++ src/routes/webinars/+page.svelte
@@ -137,29 +137,34 @@
         display: flex;
         flex-direction: column;
         align-items: baseline;
-        gap: 1em;
+        /* gap: 1em; */
+        gap: 2em 3em;
         max-width: 60em;
         display: flex;
         justify-items: center;
         width: fit-content;
         margin-left: auto;
         margin-right: auto;
+        padding: 0em 0em 3em 0em;
 
-        @media (min-width: 600px) {
+        @media (min-width: 650px) {
             display: grid;
             grid-template-rows: auto;
             grid-template-columns: 1fr 1fr;
         }
 
-        @media (min-width: 900px) {
+        @media (min-width: 1000px) {
             grid-template-columns: 1fr 1fr 1fr;
             justify-self: center;
         }
 
-        @media (min-width: 1200px) {
+         @media (min-width: 1500px) {
             grid-template-columns: 1fr 1fr 1fr 1fr;
             justify-content: center;
+        }
 
+        @media (min-width: 1920px) {
+            grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
         }
     } 
 
