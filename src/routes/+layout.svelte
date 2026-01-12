af0e35af485e8d72bd64ab294fb66b719f3ce638
nadiachaja <nadiagaja13@gmail.com>
2026-01-12 12:42:30 +0100

    fix: #390 header 2 keer nu 1 keer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
     import logoKemenkesMobile from "$lib/assets/Kemenkes-logo-mobile.png";
     import open from "$lib/assets/hamburger-menu.svg";
     import close from "$lib/assets/close-menu.svg";
+    import Header from "$lib/organisms/header.svelte";
     let { children } = $props();
 </script>
 
@@ -20,63 +21,7 @@
     </style>
 </svelte:head>
 
-<header>
-    <div class="logo-hospitals">
-        <a class="logo-avl" href={`/`}>
-            <img
-                class="avl-desktop-logo"
-                src={logoAvlDesktop}
-                alt="logo AVL"
-                height="50"
-                width="150"
-            />
-            <img
-                class="avl-mobile-logo"
-                src={logoAvlMobile}
-                alt="logo AVL"
-                height="50"
-                width="50"
-            />
-        </a>
-
-        <a class="logo-kemenkes" href={`/`}>
-            <img
-                class="kemenkes-desktop-logo"
-                src={logoKemenkesDesktop}
-                alt="logo Kemenkes"
-                height="60"
-                width="150"
-            />
-            <img
-                class="kemenkes-mobile-logo"
-                src={logoKemenkesMobile}
-                alt="logo Kemenkes"
-                height="50"
-                width="50"
-            />
-        </a>
-    </div>
-
-    <a href="#menu" class="menu-toggle"
-        ><img src={open} alt="open menu" height="50" width="50" /></a
-    >
-
-    <nav class="nav-header" id="menu">
-        <a href="#" class="close"><img src={close} alt="close menu" height="50" width="50" /></a>
-        <ul>
-            <li><a class="webinars" href={`/webinars`}>Webinars</a></li>
-            <li><a class="contourings" href={`/contourings`}>Contourings</a></li>
-            <li><a class="speakers" href={`/speakers`}>Speakers</a></li>
-            <li><a class="profile" href={`/profile`}>Profile</a></li>
-        </ul>
-
-        <select id="language" name="language">
-            <option value="language">Language</option>
-            <option value="English">English</option>
-            <option value="Indonesian">Indonesian</option>
-        </select>
-    </nav>
-</header>
+<Header></Header>
 
 {@render children?.()}
 
