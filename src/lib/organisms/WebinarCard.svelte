4fe2f6813883ed9db29032bd07ae9e8c737b7b86
nadiachaja <nadiagaja13@gmail.com>
2026-01-13 16:00:56 +0100

    refactor: #396 organisme

---
+++ src/lib/organisms/WebinarCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { data } = $props();
+    import { ContainerCard} from '$lib/index.js'
+</script>
+
+<div class="container-cards">
+    {#each webinars as webinar}
+
+    <ContainerCard></ContainerCard>
+
+    {/each}
+</div>
