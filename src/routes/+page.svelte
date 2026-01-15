351309ceeeaef50d3aada399d3d9e1e0f9b13770
Sidopjescherm <hermussidney@gmail.com>
2026-01-15 11:16:43 +0100

    #397 Plaatsen footer component
    om alvast het component neer te zetten gebruik ik alvast het Footer component in de +page.svelte waar ook de footer staat.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
     import Header from "$lib/organisms/header.svelte";
     import Article from "$lib/organisms/article.svelte";
     import Carrousel from "$lib/organisms/logo-carrousel.svelte";
+    import Footer from "$lib/organisms/footer.svelte";
 
 
     /** @type {{ data: import('./$types').PageData }} */
@@ -102,6 +103,7 @@
             {/each}
         </section>
     </section>
+    <Footer></Footer>
 </main>
 
 <style>
