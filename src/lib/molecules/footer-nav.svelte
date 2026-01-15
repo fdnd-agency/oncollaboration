8661eca8fa23b52bc95838bad86a85ef55e3e48c
Sidopjescherm <hermussidney@gmail.com>
2026-01-15 12:24:33 +0100

    #397 Navigatie Links
    In de footer komen ook de links voor de navigatie. Vandaar dat ik hier de strong en de footerlinks inlaadt zodat die gebruikt kunnen worden om makkelijk door de pagina te navigeren.

---
+++ src/lib/molecules/footer-nav.svelte
@@ -0,0 +1,28 @@
+<script>
+    import "$lib/global.css"
+    import FooterUlLinks from "$lib/atoms/footer-ul-links.svelte"
+    import Strong from "$lib/atoms/strong.svelte"
+</script>
+
+<svelte:head>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+    </style>
+</svelte:head>
+
+<nav>
+    <Strong>
+        Navigation
+    </Strong>
+    <FooterUlLinks></FooterUlLinks>
+</nav>
+
+<style>
+    nav {
+        display: grid;
+    }
+</style>
