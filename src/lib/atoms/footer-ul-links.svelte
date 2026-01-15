2d881108c9c9037699f25fa1b4a54dcba544c3ed
Sidopjescherm <hermussidney@gmail.com>
2026-01-15 12:31:14 +0100

    #397 footer ul links
    Na het opdelen van de links in de footer is dit voor de navigatie. Nu waarom het geen navigatie heet is, omdat ik een component Footernav heb en deze links daarin toe behoren.

---
+++ src/lib/atoms/footer-ul-links.svelte
@@ -0,0 +1,29 @@
+<script>
+    import "$lib/global.css";
+</script>
+
+<ul>
+    <li><a class="webinars" href={`/webinars`}>Webinars</a></li>
+    <li><a class="contourings" href={`/contourings`}>Contourings</a></li>
+    <li><a class="speakers" href={`/speakers`}>Speakers</a></li>
+    <li><a class="profile" href={`/profile`}>Profile</a></li>
+</ul>
+
+<style>
+    ul {
+        display: grid;
+    }
+
+    li, a {
+        color: var(--neutral-color-lightest);
+    }
+
+    li, a{
+        font-family: var(--primary-font-family);
+        font-style: var(--secondary-font-weight);
+    }
+
+    a:focus {
+        border: 0 0 0 2px var(--primary-color-true-aqua);
+    }
+</style>
