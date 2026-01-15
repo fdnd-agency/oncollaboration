f40e685fd78fb111d8c289f75f2d8ec2d5fb9976
Sidopjescherm <hermussidney@gmail.com>
2026-01-15 11:14:26 +0100

    #397 Opdelen footer component
    In het Address staat ook een strong met de contact informatie. Ik vond dat dit wel een atoom is, omdat je als er ooit een contact pagina komt je het hiervoor kan gebruiken.

---
+++ src/lib/atoms/contactInfoStrong.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "$lib/global.css"
+</script>
+
+<strong>Contact Information</strong>
+
+<style>
+    strong {
+        color: var(--neutral-color-lightest);
+        font-family: var(--primary-font-family);
+        font-style: var(--secondary-font-weight);
+    }
+</style>
