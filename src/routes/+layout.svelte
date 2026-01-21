9d75a667385985fbadc616f83c0c06aa14d31fb1
nadiachaja <nadiagaja13@gmail.com>
2026-01-21 16:03:38 +0100

    fix: skip link weer toegevoegd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,28 @@
     let { children } = $props();
 </script>
 
+<a href="#main-content" class="skip-link"> Skip to content </a>
 <Header></Header>
+<main id="main-content">
 {@render children?.()}
+</main>
 <Footer></Footer>
 
+
+<style>
+        .skip-link {
+        position: absolute;
+        background-color: var(--primary-color-aqua-dark-3);
+        color: var(--primary-color-blue-dark-2);
+        border-radius: var(--border-radius-small);
+        padding: 1em;
+        margin: 1em;
+        text-decoration: none;
+        z-index: 1000;
+        transform: translateY(-200%);
+        transition: transform 0.3s;
+        &:focus {
+            transform: translate(0%);
+        }
+    }
+</style>
