7da81e361ab0a94399f50a6082190ce21017ea28
Sidopjescherm <hermussidney@gmail.com>
2026-01-22 14:33:47 +0100

    #410 Comment uitleg
    Om ervoor te zorgen dat je steeds kan testen op fouten ALS er iets gebeurt heb ik ervoor gezord dat je alleen de code hoeft te 'uncommenten.'

--- src/routes/webinars/[slug]/+page.server.js
+++ src/routes/webinars/[slug]/+page.server.js
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit'
 
 export async function load({params}) {
         const webinarURL = await fetch (`https://fdnd-agency.directus.app/items/avl_webinars?fields=slug,thumbnail.id,thumbnail.title,duration,title,video,speakers.avl_speakers_id.fullname,categories.avl_categories_id.name,date&filter[slug][_eq]=${params.slug}`)
-        // const categoriesURL = await fetch(`https://fdnd-agency.directus.app/items/avl_categories`)
+        // If ever there is an issue simply uncomment the console log and see where the issue lies
         // console.log(webinarURL)
         // console.log(categoriesURL)
         if (!webinarURL.ok) {
@@ -11,7 +11,6 @@ export async function load({params}) {
         
         const webinar = await webinarURL.json()
 
-        // const categories = await categoriesURL.json()
         // console.log(webinar)
         return { webinar: webinar.data[0] }
 }
